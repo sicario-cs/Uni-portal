c62af81c284c73bef7e8122f44f9478debb68023
Ahmad Joba <ahmadjoba2005@gmail.com>
2025-11-21 14:01:52 +0200

    some edit

--- server/index.js
+++ server/index.js
@@ -7,6 +7,52 @@ require("./src/config/db.neo4j");
 // Import Express app
 const app = require("./src/app");
 
+<<<<<<< Updated upstream
+=======
+
+  
+// Redis
+const redisClient = redis.createClient({
+  url: "redis://redis_db:6379"
+});
+redisClient.on("connect", () => {
+  console.log("Redis connected");
+});
+redisClient.on("error", (err) => {
+  console.log("Redis error:", err);
+});
+
+redisClient.connect();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Cassandra
+const cassandraClient = new cassandra.Client({
+  contactPoints: ["cassandra"],
+  localDataCenter: "datacenter1"
+});
+cassandraClient.connect()
+  .then(() => console.log("Cassandra connected"))
+  .catch(err => console.error(err));
+
+// Neo4j
+const neoDriver = neo4j.driver(
+  "bolt://neo4j:7687",
+  neo4j.auth.basic("neo4j", "password")
+);
+neoDriver.verifyConnectivity()
+  .then(() => console.log("Neo4j connected"))
+  .catch(err => console.error(err));
+
+
+app.get("/hi", (req, res) => {
+  res.send("Hello ahmad");
+});
+
+ 
+>>>>>>> Stashed changes
 // Server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
